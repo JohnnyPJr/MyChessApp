ad734264a57556ddee49c5801479552a1d8f38f4
Ioannis Platsis <i.platsis@afse.eu>
2020-09-13 14:20:22 +0300

    empty project creation

---
+++ MyChessApp/MyChessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyChessApp
+//
+//  Created by John Platsis on 13/9/20.
+//  Copyright © 2020 IPV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
